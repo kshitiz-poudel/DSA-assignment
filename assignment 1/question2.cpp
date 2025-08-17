2a4a76ba1ec0d8b9c51747fac679554203ad4170
kshitiz-poudel <kshitizpoudel353@gmail.com>
2025-08-18 00:08:07 +0530

    fixed duplicate removal logic

--- assignment 1/question2.cpp
+++ assignment 1/question2.cpp
@@ -1,26 +1,38 @@
-//Design the logic to remove the duplicate elements from an Array and after the deletion the array should containtheunique elements.
+//Design the logic to remove the duplicate elements from an Array and after the deletion the array should contain the unique elements.
 
 #include<iostream>
 using namespace std;
+int main()
+{
+    int arr[100],n,i,j,k;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    cout<<"Enter the elements of the array: ";
+    for(i=0;i<n;i++)
+     {
 
-int main (){
-  int n, i, j, k;
-  cin >> n;
-  int arr[n];
-  for(i=0;i<n;i++){
-    cin >> arr[i];
-  }
-  
-  for(i=0;i<n;i++){
-    for(j=i+1;j<n;j++){
-      if(arr[i] == arr[j]){
-        arr[j] = arr[j+1];
-        n--;
+        cin>>arr[i];
+     }
+     for(i=0;i<n;i++)
+     {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    arr[k]=arr[k+1];
+
+                }
+                n--;
+        j--;
+            }
+        }
+     }
+      cout<<"array after removing duplicates: ";
+      for(i=0;i<n;i++) 
+      {
+            cout<<arr[i]<<" ";
       }
-    }
-  }
-  for(i=0;i<n;i++){
-    cout << arr[i];
-  }
-  return 0;
+      return 0;  
 }
